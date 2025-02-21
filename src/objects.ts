988e98d157cf5f72411be72c8c3a4d0fe8b8b644
Brendan van Riper <bfvr@udel.edu>
2025-02-21 16:44:16 -0500

    complete task 6

--- src/objects.ts
+++ src/objects.ts
@@ -1,16 +1,26 @@
 import { Question, QuestionType } from "./interfaces/question";
 
 /**
- * Create a new blank question with the given `id`, `name`, and `type. The `body` and
- * `expected` should be empty strings, the `options` should be an empty list, the `points`
- * should default to 1, and `published` should default to false.
+ * Create a new blank question with the given `id`, `name`, and `type.` The
+ * `body` and `expected` should be empty strings, the `options` should be an
+ * empty list, the `points` should default to 1, and `published` should default
+ * to false.
  */
 export function makeBlankQuestion(
     id: number,
     name: string,
-    type: QuestionType
+    type: QuestionType,
 ): Question {
-    return {};
+    return {
+        id: id,
+        name: name,
+        type: type,
+        body: "",
+        expected: "",
+        options: [],
+        points: 1,
+        published: false,
+    };
 }
 
 /**
@@ -21,17 +31,20 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    return (
+        question.expected.toLowerCase().trim() === answer.toLowerCase().trim()
+    );
 }
 
 /**
  * Consumes a question and a potential `answer`, and returns whether or not
- * the `answer` is valid (but not necessarily correct). For a `short_answer_question`,
- * any answer is valid. But for a `multiple_choice_question`, the `answer` must
- * be exactly one of the options.
+ * the `answer` is valid (but not necessarily correct). For a
+ * `short_answer_question`, any answer is valid. But for a
+ * `multiple_choice_question`, the `answer` must be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    if (question.type === "short_answer_question") return true;
+    return question.options.findIndex((x) => x === answer) !== -1;
 }
 
 /**
@@ -41,7 +54,7 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    return question.id.toString() + ": " + question.name.slice(0, 10);
 }
 
 /**
@@ -62,7 +75,9 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let markdown: string[] = ["# " + question.name, question.body];
+    question.options.forEach((x) => markdown.push("- " + x));
+    return markdown.join("\n");
 }
 
 /**
@@ -70,7 +85,7 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    return question;
+    return { ...question, options: [...question.options], name: newName };
 }
 
 /**
@@ -79,17 +94,28 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    return {
+        ...question,
+        options: [...question.options],
+        published: !question.published,
+    };
 }
 
 /**
- * Create a new question based on the old question, copying over its `body`, `type`,
- * `options`, `expected`, and `points` without changes. The `name` should be copied
- * over as "Copy of ORIGINAL NAME" (e.g., so "Question 1" would become "Copy of Question 1").
- * The `published` field should be reset to false.
+ * Create a new question based on the old question, copying over its `body`,
+ * `type`, `options`, `expected`, and `points` without changes. The `name`
+ * should be copied over as "Copy of ORIGINAL NAME" (e.g., so "Question 1"
+ * would become "Copy of Question 1"). The `published` field should be reset to
+ * false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    return {
+        ...oldQuestion,
+        options: [...oldQuestion.options],
+        id: id,
+        name: "Copy of " + oldQuestion.name,
+        published: false,
+    };
 }
 
 /**
@@ -100,22 +126,33 @@ export function duplicateQuestion(id: number, oldQuestion: Question): Question {
  * Check out the subsection about "Nested Fields" for more information.
  */
 export function addOption(question: Question, newOption: string): Question {
-    return question;
+    return {
+        ...question,
+        options: [...question.options, newOption],
+    };
 }
 
 /**
  * Consumes an id, name, and two questions, and produces a new question.
- * The new question will use the `body`, `type`, `options`, and `expected` of the
- * `contentQuestion`. The second question will provide the `points`.
+ * The new question will use the `body`, `type`, `options`, and `expected` of
+ * the `contentQuestion`. The second question will provide the `points`.
  * The `published` status should be set to false.
  * Notice that the second Question is provided as just an object with a `points`
- * field; but the function call would be the same as if it were a `Question` type!
+ * field; but the function call would be the same as if it were a `Question`
+ * type!
  */
 export function mergeQuestion(
     id: number,
     name: string,
     contentQuestion: Question,
-    { points }: { points: number }
+    { points }: { points: number },
 ): Question {
-    return contentQuestion;
+    return {
+        ...contentQuestion,
+        options: [...contentQuestion.options],
+        id: id,
+        name: name,
+        points: points,
+        published: false,
+    };
 }
